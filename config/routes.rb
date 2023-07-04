6b2fe2b3ca2809936920997dcc52904811403783
Allan Losenge <temlosenge@gmail.com>
2023-07-04 22:21:11 +0300

    Added sessions controller with authentication actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   resources :categories
   resources :items
-  resources :users, only: [:create, :show, :index]
+  post '/login',    to: 'sessions#create'
+  post '/logout',   to: 'sessions#destroy'
+  get '/logged_in', to: 'sessions#is_logged_in?'
+  resources :users, only: [:create, :show, :index] do 
+    resources :items, only: [:create, :show, :index, :destroy]
+ end
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
