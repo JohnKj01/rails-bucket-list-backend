1c5507e88db353193ee4a36522a787ed1b15d11d
kk <87941966+kelvinkirui@users.noreply.github.com>
2023-07-04 22:14:03 +0300

    Added index and show

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,22 @@
 class CategoriesController < ApplicationController
-end
+    def index
+      categories = Category.all.to_json(include: :items)
+      render json: categories
+    end
+  
+    def show
+      category = Category.find_by(name: params[:name])
+      if category
+        items = category.items.to_json(include: :category)
+        render json: items
+      else
+        render json: { errors: "Category not found" }, status: :not_found
+      end
+    end
+  
+    private
+  
+    def category_params
+      params.require(:category).permit(:name)
+    end
+  end
