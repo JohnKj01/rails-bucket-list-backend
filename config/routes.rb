ac1f5cb9fa6ce5e4c7d25a5ab37cae49c5fc0ec8
Allan Losenge <temlosenge@gmail.com>
2023-07-04 22:16:42 +0300

    Added users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :categories
   resources :items
+  resources :users, only: [:create, :show, :index]
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
