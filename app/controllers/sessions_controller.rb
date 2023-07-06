784ffb776f04173ba50b1057fea4142261a3a041
Samuel <samnmbuguah@gmail.com>
2023-07-06 07:12:13 +0300

    authorization with jwt

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,4 @@
+require 'jwt'
 class SessionsController < ApplicationController
   skip_before_action :authorize_request, only: [:is_logged_in?, :create]
 
@@ -5,19 +6,20 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:email])
     if user&.authenticate(params[:password])
       session[:user_id] = user.id
-      render json: user, status: :ok, serializer: UserSerializer
+      token = generate_token(user.id)
+      render json: { token: token, user: user, expires_at: Time.now + 24.hours }, status: :ok
     else
-      render json: { error: "Invalid email or password" }, status: :unauthorized
+      render json: { error: 'Invalid email or password' }, status: :unauthorized
     end
   end
 
   def destroy
     session[:user_id] = nil
-    render json: { message: "Logged out successfully" }
+    render json: { message: 'Logged out successfully' }
   end
 
   def is_logged_in?
-    if current_user
+    if current_user || session[:user_id].present?
       render json: { logged_in: true, user: current_user }
     else
       render json: { logged_in: false }
@@ -28,5 +30,11 @@ class SessionsController < ApplicationController
 
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def generate_token(user_id)
+    payload = { user_id: user_id, exp: Time.now.to_i + 24.hours }
+    secret_key = Rails.application.secrets.secret_key_base
+    JWT.encode(payload, secret_key)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,7 @@ Rails.application.routes.draw do
   post '/logout',   to: 'sessions#destroy'
   get '/logged_in', to: 'sessions#is_logged_in?'
   resources :users, only: [:create, :show, :index] do 
-    resources :items, only: [:create, :show, :index, :destroy]
+    resources :items, only: [:create, :show, :patch, :index, :destroy]
  end
 
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
