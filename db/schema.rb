bd596d9fe0d64b2983ab9baf7782d618319df883
Samuel <samnmbuguah@gmail.com>
2023-07-07 08:53:47 +0300

    started on progress tracking

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_05_192407) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_07_052842) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -24,6 +24,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_05_192407) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.date "completed_by"
     t.index ["category_id"], name: "index_items_on_category_id"
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 puts "🌱 Seeding Lists..."
+
 # Seed your database here
 travel = Category.create(name: "travel")
 lifestyle = Category.create(name: "lifestyle")
@@ -8,25 +9,25 @@ user1 = User.create(name: "John Doe", email: "john@example.com", password: "pass
 user2 = User.create(name: "Jane Smith", email: "jane@example.com", password: "password")
 user3 = User.create(name: "Mike Johnson", email: "mike@example.com", password: "password")
 
-Item.create(name: "Travel to Amalfi Coast, Italy", completed: false, category: travel, user_id: user1.id)
-Item.create(name: "Travel to Santorini, Greece", completed: true, category: travel, user_id: user1.id)
-Item.create(name: "Road trip on The Wild Atlantic Way, Ireland's coastal route", completed: false, category: travel, user_id: user2.id)
-Item.create(name: "Visit the Yellowstone National Park", completed: false, category: travel, user_id: user2.id)
-Item.create(name: "Visit the Glacier national park", completed: true, category: travel, user_id: user3.id)
-Item.create(name: "Visit Alaska to see Northern lights", completed: false, category: travel, user_id: user3.id)
+Item.create(name: "Travel to Amalfi Coast, Italy", completed: false, completed_by: Date.new(2023, 12, 31), category: travel, user_id: user1.id)
+Item.create(name: "Travel to Santorini, Greece", completed: true, completed_by: Date.new(2023, 9, 30), category: travel, user_id: user1.id)
+Item.create(name: "Road trip on The Wild Atlantic Way, Ireland's coastal route", completed: false, completed_by: Date.new(2023, 11, 30), category: travel, user_id: user2.id)
+Item.create(name: "Visit the Yellowstone National Park", completed: false, completed_by: Date.new(2024, 1, 31), category: travel, user_id: user2.id)
+Item.create(name: "Visit the Glacier national park", completed: true, completed_by: Date.new(2023, 10, 31), category: travel, user_id: user3.id)
+Item.create(name: "Visit Alaska to see Northern lights", completed: false, completed_by: Date.new(2023, 12, 31), category: travel, user_id: user3.id)
 
-Item.create(name: "Meal prep every Sunday", completed: false, category: lifestyle, user_id: user1.id)
-Item.create(name: "Go to the gym and lift heavy 3 times a week", completed: true, category: lifestyle, user_id: user1.id)
-Item.create(name: "Yoga and stretching twice a week", completed: false, category: lifestyle, user_id: user2.id)
-Item.create(name: "Learn a new language", completed: false, category: lifestyle, user_id: user2.id)
-Item.create(name: "Talk to parents once a week", completed: true, category: lifestyle, user_id: user3.id)
-Item.create(name: "Read 1 book a month", completed: false, category: lifestyle, user_id: user3.id)
+Item.create(name: "Meal prep every Sunday", completed: false, completed_by: Date.new(2023, 9, 30), category: lifestyle, user_id: user1.id)
+Item.create(name: "Go to the gym and lift heavy 3 times a week", completed: true, completed_by: Date.new(2023, 12, 31), category: lifestyle, user_id: user1.id)
+Item.create(name: "Yoga and stretching twice a week", completed: false, completed_by: Date.new(2023, 10, 31), category: lifestyle, user_id: user2.id)
+Item.create(name: "Learn a new language", completed: false, completed_by: Date.new(2023, 11, 30), category: lifestyle, user_id: user2.id)
+Item.create(name: "Talk to parents once a week", completed: true, completed_by: Date.new(2023, 12, 31), category: lifestyle, user_id: user3.id)
+Item.create(name: "Read 1 book a month", completed: false, completed_by: Date.new(2023, 9, 30), category: lifestyle, user_id: user3.id)
 
-Item.create(name: "Go to pottery class", completed: false, category: experience, user_id: user1.id)
-Item.create(name: "Try Curling", completed: true, category: experience, user_id: user1.id)
-Item.create(name: "Bungee jumping", completed: false, category: experience, user_id: user2.id)
-Item.create(name: "Try white water rafting", completed: false, category: experience, user_id: user2.id)
-Item.create(name: "See a live musical", completed: true, category: experience, user_id: user3.id)
-Item.create(name: "Learn how to make Cannoli", completed: false, category: experience, user_id: user3.id)
+Item.create(name: "Go to pottery class", completed: false, completed_by: Date.new(2023, 10, 31), category: experience, user_id: user1.id)
+Item.create(name: "Try Curling", completed: true, completed_by: Date.new(2023, 12, 31), category: experience, user_id: user1.id)
+Item.create(name: "Bungee jumping", completed: false, completed_by: Date.new(2023, 11, 30), category: experience, user_id: user2.id)
+Item.create(name: "Try white water rafting", completed: false, completed_by: Date.new(2023, 12, 31), category: experience, user_id: user2.id)
+Item.create(name: "See a live musical", completed: true, completed_by: Date.new(2023, 9, 30), category: experience, user_id: user3.id)
+Item.create(name: "Learn how to make Cannoli", completed: false, completed_by: Date.new(2023, 10, 31), category: experience, user_id: user3.id)
 
 puts "✅ Done seeding!"
