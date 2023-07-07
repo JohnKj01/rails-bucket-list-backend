c496a7caf65f9e7728d57c379e67497271e22ac5
Samuel <samnmbuguah@gmail.com>
2023-07-07 04:55:52 +0300

    items update method

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,24 +9,24 @@ user2 = User.create(name: "Jane Smith", email: "jane@example.com", password: "pa
 user3 = User.create(name: "Mike Johnson", email: "mike@example.com", password: "password")
 
 Item.create(name: "Travel to Amalfi Coast, Italy", completed: false, category: travel, user_id: user1.id)
-Item.create(name: "Travel to Santorini, Greece", completed: false, category: travel, user_id: user1.id)
+Item.create(name: "Travel to Santorini, Greece", completed: true, category: travel, user_id: user1.id)
 Item.create(name: "Road trip on The Wild Atlantic Way, Ireland's coastal route", completed: false, category: travel, user_id: user2.id)
 Item.create(name: "Visit the Yellowstone National Park", completed: false, category: travel, user_id: user2.id)
-Item.create(name: "Visit the Glacier national park", completed: false, category: travel, user_id: user3.id)
+Item.create(name: "Visit the Glacier national park", completed: true, category: travel, user_id: user3.id)
 Item.create(name: "Visit Alaska to see Northern lights", completed: false, category: travel, user_id: user3.id)
 
 Item.create(name: "Meal prep every Sunday", completed: false, category: lifestyle, user_id: user1.id)
-Item.create(name: "Go to the gym and lift heavy 3 times a week", completed: false, category: lifestyle, user_id: user1.id)
+Item.create(name: "Go to the gym and lift heavy 3 times a week", completed: true, category: lifestyle, user_id: user1.id)
 Item.create(name: "Yoga and stretching twice a week", completed: false, category: lifestyle, user_id: user2.id)
 Item.create(name: "Learn a new language", completed: false, category: lifestyle, user_id: user2.id)
-Item.create(name: "Talk to parents once a week", completed: false, category: lifestyle, user_id: user3.id)
+Item.create(name: "Talk to parents once a week", completed: true, category: lifestyle, user_id: user3.id)
 Item.create(name: "Read 1 book a month", completed: false, category: lifestyle, user_id: user3.id)
 
 Item.create(name: "Go to pottery class", completed: false, category: experience, user_id: user1.id)
-Item.create(name: "Try Curling", completed: false, category: experience, user_id: user1.id)
+Item.create(name: "Try Curling", completed: true, category: experience, user_id: user1.id)
 Item.create(name: "Bungee jumping", completed: false, category: experience, user_id: user2.id)
 Item.create(name: "Try white water rafting", completed: false, category: experience, user_id: user2.id)
-Item.create(name: "See a live musical", completed: false, category: experience, user_id: user3.id)
+Item.create(name: "See a live musical", completed: true, category: experience, user_id: user3.id)
 Item.create(name: "Learn how to make Cannoli", completed: false, category: experience, user_id: user3.id)
 
 puts "✅ Done seeding!"
