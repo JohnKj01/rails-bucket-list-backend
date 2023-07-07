2e2fdb103b36e704943c78286f04979819b23488
Samuel <samnmbuguah@gmail.com>
2023-07-07 18:36:37 +0300

    Misc

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:4000', 'https://front-q8u9iaboo-kelvinkirui.vercel.app'
+    origins 'http://localhost:4000'
     resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options], credentials: true
   end
 end
