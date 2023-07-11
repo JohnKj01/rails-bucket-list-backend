bcb081648135d45f5e0b8d2f3b04b1e272c556e4
Allan Losenge <temlosenge@gmail.com>
2023-07-11 14:38:15 +0300

    Cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:4000'
+    origins 'https://front-q8u9iaboo-kelvinkirui.vercel.app'
     resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options], credentials: true
   end
 end
